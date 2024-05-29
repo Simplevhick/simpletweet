66f2053b248d6f827d9ba67627a248dbf83419eb
simplevhick <chimborvictor10@gmail.comgit config --global user.email chimborvictor10@gmail.com>
2024-05-29 11:26:17 +0100

    Added comments component and updated PostPage to display comments

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -3,6 +3,7 @@ import { doc, getDoc, getFirestore } from "firebase/firestore";
 import { HiArrowLeft } from "react-icons/hi";
 import Link from "next/link";
 import Post from "@/components/Post";
+import Comments from "@/components/Comments";
 
 export default async function PostPage({ params }) {
   const db = getFirestore(app);
@@ -20,6 +21,7 @@ export default async function PostPage({ params }) {
       <h2 className="sm:text-lg">Back</h2>
     </div>
     <Post post={data} id={data.id}/>
+    <Comments id={params.id} />
   </div>
   )
 }
